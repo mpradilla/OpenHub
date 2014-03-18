6dc3fbb871ec87b4bda0e61889de79162790edd0
mpradilla <maurizip@gmail.com>
2014-03-17 21:29:49 -0500

    smal fixes

--- worker/zFileTemp/gnip-java-master/src/main/java/com/gnipcentral/client/resource/Resource.java
+++
@@ -1,7 +0,0 @@
-package com.gnipcentral.client.resource;
-
-/**
- * Interface that marks classes as Gnip model objects.
- */
-public interface Resource {
-}
--- worker/zFileTemp/gnip-java-master/src/main/java/com/gnipcentral/client/util/HTTPMethod.java
+++
@@ -1,8 +0,0 @@
-package com.gnipcentral.client.util;
-
-/**
- * Package-protected abstraction for enumerating HTTP verbs.  This class is not intended for external use.
- */
-enum HTTPMethod {
-    GET, POST, PUT, DELETE
-}
